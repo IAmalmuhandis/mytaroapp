eb79205ac5597f99dd015f3a7f498f10ae90b455
Kafeel Kazeem <kafeelkazeem@gmail.com>
2024-04-24 18:08:23 +0100

    new changes

--- src/pages/firstPage.jsx
+++ src/pages/firstPage.jsx
@@ -11,51 +11,65 @@ import SearchIcon from '@mui/icons-material/Search';
 
 const FirstPage = () => {
   return (
-    <Container>
-      <Stack spacing={2} direction="row" mx={1} mt={7} justifyContent='space-between'>
+    <Container sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+      <Stack spacing={2} direction="row" mx={1} mt={{ xs: 3, sm: 5, md: 7 }} justifyContent='space-between'>
         <Logo />
         <SigninBtn /> 
       </Stack>
-      <Container sx={{marginTop: '50px'}}>
-        <Typography sx={{fontFamily: 'Montserrat', fontSize: '21px', lineHeight: '40px', color: blue, fontWeight: '800', letterSpacing: '1%'}}>What Type Of Event Are You Planning?</Typography>
+      <Container>
+        <Typography sx={{ 
+          fontFamily: 'Montserrat', 
+          fontSize: '21px', 
+          lineHeight: '40px', 
+          color: blue, 
+          fontWeight: '800', 
+          letterSpacing: '1%', 
+          textAlign: 'center' 
+          }}
+          mt={3}
+          >What Type Of Event Are You Planning?
+        </Typography>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Arrow />
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Anniversary />
+        </div>
       </Container>
-      <Container sx={{paddingLeft: '49px'}}>
-        <Arrow />
-      </Container>
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '-40px' }}>
-        <Anniversary />
-      </div>
       <Stack spacing={2} direction="column" mt={-3} alignItems="center">
         <BlueBtn title='Wedding' />
         <BlueBtn title='Conference' />
         <BlueBtn title='Birthday' />
+        <BlueBtn title='Concert' />
       </Stack>
-      <Typography sx={{marginTop: '10px', color: blue, textAlign: 'center', fontWeight: '800', fontFamily: 'Montserrat', fontSize: '9px', lineHeight: '28.4px', letterSpacing: '1%'}}>Did'nt find answer?</Typography>
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '5px' }}>
-      <TextField
-        sx={{
-            backgroundColor: '#E7E7E7',
-            borderRadius: '20.29px',
-            fontWeight: '8.77px',
-            '& .MuiOutlinedInput-root': {
-                height: '35px', // Set the height
+      <Stack spacing={2} direction="column" alignItems="center" mt={2} mb={2}>
+        <Typography sx={{ color: blue, textAlign: 'center', fontWeight: '800', fontFamily: 'Montserrat', fontSize: '9px', lineHeight: '28.4px', letterSpacing: '1%' }}>Didn't find the answer?</Typography>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <TextField
+            sx={{
+              backgroundColor: '#E7E7E7',
+              borderRadius: '20.29px',
+              fontWeight: '8.77px',
+              '& .MuiOutlinedInput-root': {
+                height: '35px', 
                 width: '194px',
                 fontSize: '12px',
                 '& fieldset': {
-                    border: 'none', // Remove the border
+                  border: 'none', 
                 },
-            },
-        }}
-        placeholder="Type something.........."
-        InputProps={{
-            startAdornment: (
+              },
+            }}
+            placeholder="Type something.........."
+            InputProps={{
+              startAdornment: (
                 <InputAdornment position="start">
-                    <SearchIcon  fontSize='12px'/>
+                  <SearchIcon fontSize='12px'/>
                 </InputAdornment>
-            ),
-        }}
-    />
-       </div>
+              ),
+            }}
+          />
+        </div>
+    </Stack>
     </Container>
   );
 }
