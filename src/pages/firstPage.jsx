943814a285d3a4609c3d5a14d3171a9bda0f3186
Kafeel Kazeem <kafeelkazeem@gmail.com>
2024-04-25 09:37:46 +0100

    new changes

--- src/pages/firstPage.jsx
+++ src/pages/firstPage.jsx
@@ -15,7 +15,9 @@ const FirstPage = () => {
   const theme = useTheme();
   const isTablet = useMediaQuery(theme.breakpoints.up('sm'))
   const isLaptopScreen = useMediaQuery(theme.breakpoints.up('md'));
-  const isLargeScreen = isTablet || isLaptopScreen 
+  const isDesktopScreen = useMediaQuery(theme.breakpoints.up('lg'));
+  const isLargeScreen = isTablet || isLaptopScreen || isDesktopScreen
+  const isSmallLaptop = window.innerWidth >= 912 && window.innerWidth <= 1024
   const typeSomtingStyle = {
     backgroundColor: '#E7E7E7',
     borderRadius: isLargeScreen ? '60px' : '20.29px',
@@ -31,10 +33,12 @@ const FirstPage = () => {
   }
   return (
     <Container maxWidth="none" sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+
       <Stack spacing={2} direction="row" mx={1} mt={{ xs: 3, sm: 5, md: 7 }} justifyContent='space-between'>
         <Logo width={isLargeScreen ? 187 : 57} height={isLargeScreen ? 109 : 57}/>
         <SigninBtn width={isLargeScreen ? '200px' : '120px'} height={isLargeScreen ? '50px' : '32px'} fontSz={isLargeScreen ? '18px' : '9px'}/> 
       </Stack>
+
       <Container>
         <Typography sx={{ 
           fontFamily: 'Montserrat', 
@@ -52,23 +56,26 @@ const FirstPage = () => {
           <Arrow width={isLargeScreen ? '193px' : '112px'} height={isLargeScreen ? '37px' : '23px'}/>
         </div>
       </Container>
+
       <Grid container justifyContent="space-around" mt={isLargeScreen ? -12 : 0}>
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Anniversary width={isLargeScreen ? 500 : 283} height={isLargeScreen ? 639 : 293}/>
         </div>
-        <Stack spacing={2} direction="column" mt={isLargeScreen && 20 }>
-          <Grid alignItems='center'>
+        <Stack spacing={2} direction="column" mt={isSmallLaptop && -10 || isLaptopScreen && 20 || isTablet && -10 || isDesktopScreen && 20 }>
+
+          <Grid alignItems='center'>  
           <Stack spacing={isLargeScreen ? 4 : 2} direction={isLargeScreen ? 'row' : 'column'}>
             <BlueBtn width={isLargeScreen ? '300px' : '200px'} height={isLargeScreen ? '70px'  : '46px'} fontSz={isLargeScreen ? '28px' : '16px'} title='Wedding' />
             <BlueBtn width={isLargeScreen ? '300px' : '200px'} height={isLargeScreen ? '70px'  : '46px'} fontSz={isLargeScreen ? '28px' : '16px'} title='Conference' />
           </Stack>
           </Grid>
+
           <Grid alignItems='center'>
           <Stack spacing={isLargeScreen ? 4 : 2} direction={isLargeScreen ? 'row' : 'column'}>
               <BlueBtn width={isLargeScreen ? '300px' : '200px'} height={isLargeScreen ? '70px'  : '46px'} fontSz={isLargeScreen ? '28px' : '16px'} title='Birthday' />
               <BlueBtn width={isLargeScreen ? '300px' : '200px'} height={isLargeScreen ? '70px'  : '46px'} fontSz={isLargeScreen ? '28px' : '16px'} title='Concert' />
-            </Stack>
-            <Stack spacing={1} direction="column" alignItems="center" mt={5}>
+          </Stack>
+          <Stack spacing={1} direction="column" alignItems="center" mt={5}>
             <Typography sx={{ color: blue, textAlign: 'center', fontWeight: '800', fontFamily: 'Montserrat', fontSize: isLargeScreen ? '22.29px' : '9px', lineHeight: '28.4px', letterSpacing: '1%' }}>Didn't find the answer?</Typography>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
             <TextField
@@ -84,7 +91,7 @@ const FirstPage = () => {
           />
           </div>
           </Stack>
-          </Grid>
+        </Grid>
       </Stack>
     </Grid>
     </Container>
